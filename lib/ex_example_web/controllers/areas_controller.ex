9e3bf10e2851d44ef334905f6bd0220f5284183b
Tashiro <g4160hc@gmail.com>
2021-08-22 00:09:14 +0900

    add areas api

--- lib/ex_example_web/controllers/areas_controller.ex
+++ lib/ex_example_web/controllers/areas_controller.ex
@@ -1,11 +1,13 @@
 defmodule ExExampleWeb.AreasController do
   use ExExampleWeb, :controller
 
-  def index(conn, params) do
-    IO.inspect(params)
+  def index(conn, _params) do
+    areas = ExExample.Repo.all ExExample.Area
     json(
       conn,
-      Map.merge(%{id: :test}, params)
+      %{
+        names: Enum.map(areas, fn(area) -> area.name end)
+      }
     )
   end
 end
